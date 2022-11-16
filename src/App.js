84b01b0a84c9722c625b5b913af1e0b7ef4a108b
Muhammad <muhammadnaqib2000@gmail.com>
2022-11-17 01:16:58 +0400

    setup login page and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import * as ROUTES from "./constants/routes";
+
+const Login = lazy(() => import("./pages/login"));
+
 function App() {
-  return <p>Hello World</p>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login}></Route>
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/login.js
@@ -0,0 +1,22 @@
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+
+export default function Login() {
+  const history = useHistory();
+  const { firbase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const isInvalid = password == "" || emailAddress == "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login - PicturGram";
+  }, []);
+
+  return <p>I am the login page</p>;
+}
