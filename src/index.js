7b4c4d29f5d53077c15f92a67a6a11c1fde334a9
Muhammad <muhammadnaqib2000@gmail.com>
2022-11-17 23:24:09 +0400

    setup tailwind

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 
 import FirebaseContext from "./context/firebase";
 import { firebase, FieldValue } from "./lib/firebase";
+import "./styles/app.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
