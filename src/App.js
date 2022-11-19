d04efda419bda3d73dc0ec00dfaaeed39b7dadbe
Muhammad <muhammadnaqib2000@gmail.com>
2022-11-20 01:03:15 +0400

    Start not-found page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const NotFound = lazy(() => import("./pages/not-found"));
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login}></Route>
           <Route path={ROUTES.SIGN_UP} component={SignUp}></Route>
+          <Route path={ROUTES.NOT_FOUND} component={NotFound}></Route>
         </Switch>
       </Suspense>
     </Router>
   );
 }
-
-export default App;
