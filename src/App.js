8c461ca3df77010592afac1af114e03a1f60cc11
Muhammad <muhammadnaqib2000@gmail.com>
2022-11-22 09:57:47 +0400

    Start dashboard and add user authenticator listener hook

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
 const NotFound = lazy(() => import("./pages/not-found"));
 
 export default function App() {
@@ -12,9 +13,10 @@ export default function App() {
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
-          <Route path={ROUTES.LOGIN} component={Login}></Route>
-          <Route path={ROUTES.SIGN_UP} component={SignUp}></Route>
-          <Route path={ROUTES.NOT_FOUND} component={NotFound}></Route>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+          <Route component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useContext } from "react";
+import FirebaseContext from "../context/firebase";
+
+export default function useAuthListener() {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("authUser"))
+  );
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStageChanged((authUser) => {
+      //if we have  a user ... therefore we can store the user in local storage
+      if (authUser) {
+        localStorage.setItem("authUser", JSON.stringify(authUser));
+        setUser(authUser);
+        //we don't have an auth user, therefore clear the local storage
+      } else {
+        localStorage.removeItem("authUser");
+        setUser(null);
+      }
+    });
+
+    return () => listener();
+  }, [firebase]);
+
+  return { user };
+}
