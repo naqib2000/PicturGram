b31cafc801874c4ea1c81323bdd8d545f442e7f8
Muhammad <muhammadnaqib2000@gmail.com>
2022-11-23 08:51:56 +0400

    Fix get user by id function refernce error

--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -1,15 +1,15 @@
 import { useState, useEffect, useContext } from "react";
 import UserContext from "../context/user";
-import { getUserObjByUserId } from "../services/firebase";
+import { getUserByUserId } from "../services/firebase";
 
-export function useUser() {
+export default function useUser() {
   const [activeUser, setActiveUser] = useState({});
   const { user } = useContext(UserContext);
 
   useEffect(() => {
     async function getUserObjByUserId() {
       //we need a function we can call (firebase service) that gets the user data based on the id
-      const response = await getUserObjByUserId(user.uid);
+      const [response] = await getUserByUserId(user.uid);
       setActiveUser(response);
     }
     if (user?.uid) {
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,7 +10,7 @@ export async function doesUsernameExist(username) {
   return result.docs.map((user) => user.data().length > 0);
 }
 
-export async function getUserObjByUserId(userId) {
+export async function getUserByUserId(userId) {
   const result = await firebase
     .firestore()
     .collection("users")
