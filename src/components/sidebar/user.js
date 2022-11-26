6690a3beffcd68f86e9ced7486e739c093dafef9
Muhammad <muhammadnaqib2000@gmail.com>
2022-11-26 08:06:17 +0400

    Add user feature for sidebar

---
+++ src/components/sidebar/user.js
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+
+export default function User ({ username, fullName, test }) {
+  return !username || !fullName ? (
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link
+      to={`/p/${username}`}
+      className="grid grid-cols-4 gp-4 mb-6 items-center"
+    >
+      <div className="flex items-center justify-between col-span-1">
+        {test}
+        <img
+          className="rounded-full w-16  flex mr-3"
+          src={`/images/avatars/${username}.png`}
+          alt=""
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
+    </Link>
+  );
+}
+
+
+
+User.propTypes = {
+  username: PropTypes.string,
+  fullName: PropTypes.string,
+  test: PropTypes.number,
+};
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import "./wdyr";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-
 import FirebaseContext from "./context/firebase";
 import { firebase, FieldValue } from "./lib/firebase";
 import "./styles/app.css";
