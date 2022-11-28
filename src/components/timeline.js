0e4ca81aa16da26200709be33de52f58c364b509
Muhammad <muhammadnaqib2000@gmail.com>
2022-11-28 15:11:30 +0400

    Add usePhotos custom hook for getting photos

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,19 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+import usePhotos from "../hooks/use-photos";
+
 export default function Timeline() {
-    return (
-      <div className="container col-span-2">
-        <p>I am the timeline</p>
-      </div>
-    );
+  // we need to get the logged users photos
+  const { photos } = usePhotos();
+  console.log("photos", photos);
+  // on loading the photos we need to use the react skeleton
+  // if we have photos we need to render them (Create a post component)
+  // if the user has no photos tell them to create some photos
+
+  return (
+    <div className="container col-span-2">
+      <p>I am the timeline</p>
+    </div>
+  );
 }
