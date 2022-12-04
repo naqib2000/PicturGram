3bc0c5479ebf869e2dfc6be0c30a469fe2a1966f
Muhammad <muhammadnaqib2000@gmail.com>
2022-12-05 00:18:15 +0400

    Add componenets to profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,11 +2,12 @@ import { useParams, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getUserByUsername } from "../services/firebase";
 import * as ROUTES from "../constants/routes";
+import Header from "../components/header";
+import UserProfile from "../components/profile";
 
 export default function Profile() {
   const { username } = useParams();
   const [user, setUser] = useState(null);
-  const [userExists, setUserExists] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
@@ -14,19 +15,19 @@ export default function Profile() {
       const user = await getUserByUsername(username);
       if (user.length > 0) {
         setUser(user[0]);
-        setUserExists(true);
       } else {
         history.push(ROUTES.NOT_FOUND);
       }
     }
 
     checkUserExists();
-    console.log("user", user);
   }, [username, history]);
 
-  return userExists ? (
+  return user?.username ? (
     <div className="bg-gray-background">
+      <Header />
       <div className="mx-auto max-w-screen-lg">{user.fullName}</div>
+      <UserProfile user={user} />
     </div>
   ) : null;
 }
