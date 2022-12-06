b2bd50035b8fdd09a71ad95ae871decbaea4bf41
Muhammad <muhammadnaqib2000@gmail.com>
2022-12-06 14:08:00 +0400

    Add header to not found page

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
-
+import Header from "../components/header";
 export default function NotFound() {
   useEffect(() => {
     document.title = "Not Found - PicturGram";
   }, []);
   return (
     <div className="bg-gray-background">
-      <div className="mx-auth">
+      <Header />
+      <div className="mx-auth max-w-screen-lg">
         <p className="text-center text-2xl">Not Found!</p>
       </div>
     </div>
